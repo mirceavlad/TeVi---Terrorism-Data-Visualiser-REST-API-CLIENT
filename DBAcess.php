198e8bd3066ce39e754dec106121ca0832d7c8ad
Octavian <octavianm@zenegant.io>
2020-04-29 21:44:05 +0300

    Implement DBAcess and upload data to DB from CSV file

--- DBAcess.php
+++ DBAcess.php
@@ -5,14 +5,14 @@ class DBAcess {
     private function __construct()
     {
         if (mysqli_connect_errno()) {
-            die ('Conn error');
+            die ('Conexiunea a esuat...');
         }
-        $this -> mysql = new mysqli (
-                             'localhost', // locatia serverului (aici, masina locala)
-                             'root',       // numele de cont
-                             '',    // parola (atentie, in clar!)
-                             'notes'   // baza de date
-                             );
+        self::$mysql = new mysqli (
+                                   'localhost', // locatia serverului (aici, masina locala)
+                                   'root',       // numele de cont
+                                   '',    // parola (atentie, in clar!)
+                                   'terrordb'   // baza de date
+                                   );
     }
     
     public static function getInstance()
@@ -22,6 +22,38 @@ class DBAcess {
             self::$instance = new DBAcess();
         }
         return self::$instance;
+    }
+    
+    //creates the ATTACKS table, having columns names as given in the $headersArray
+    public function createTerrorTable($headersArray) {
+        $queryHeaders = "";
+        foreach($headersArray as $columnName){
+            $queryHeaders = $queryHeaders. " " .$columnName. " TEXT,";
+        }
+        $queryHeaders = substr($queryHeaders, 0, -1);
+        
+        if(!self::$mysql -> query('CREATE TABLE ATTACKS ('.$queryHeaders.')')) {
+           printf("%s",self::$mysql->error);
+        }
+    }
+    
+    //adds an attak to the attaks table
+    //@param $attackDataMap, an array acting as a map, mapping the each column name with a value,
+    //where the key value(array index) should be a string representing the columnName and the value
+    //should be a string representing the value to be added at the respective column
+    public function addAttack(array $attakDataMap) {
+        $columnsToInsert = '';
+        $valuesToInsert = '';
+        foreach ($attakDataMap as $key => $value) {
+            $columnsToInsert = $columnsToInsert."".$key.",";
+            $valuesToInsert = $valuesToInsert."'".$value."',";
+        }
+        $columnsToInsert = substr($columnsToInsert, 0, -1);
+        $valuesToInsert = substr($valuesToInsert, 0, -1);
+        echo 'INSERT INTO ATTACKS('.$columnsToInsert.') VALUES('.$valuesToInsert.')';
+        if(!self::$mysql -> query('INSERT INTO ATTACKS('.$columnsToInsert.') VALUES('.$valuesToInsert.')')) {
+            printf("%s",self::$mysql->error);
+        }
     }
 }
 ?>
--- csvParser.php
+++ csvParser.php
@@ -1,11 +1,40 @@
 
 <?php
-    $row = 1;
+    include 'DBAcess.php';
+    $index = 0;
+    $headers = array();
+    $values = array();
+    $headersCount = 0;
+    //opening the csv
     if (($handle = fopen("terror.csv", "r")) !== FALSE) {
-      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $row++;
-          echo "year: " .$data[1]. "   location: " .$data[7]. "\n";
+    //reading line by line from the csv
+      while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+          //echo "START";
+           $headersCount = 0;
+          //iterating through current line columns
+          foreach($data as $value) {
+              //if index == 0. generate table columns names
+            //  echo $headersCount." \n";
+              if ($index == 0) {
+                  $headers[$headersCount] = $value;
+              } else if($headersCount <= 134) {
+                  //generate data to be added to the database
+                  $indexInHead = $headers[$headersCount];
+                  $values[$indexInHead] = $value;
+              }
+              $headersCount = $headersCount + 1;
+          }
+          if($index == 0) {
+              //create table
+              DBAcess::getInstance()::createTerrorTable($headers);
+          }
+          if(!($index == 0)) {
+              // add a row
+              DBAcess::getInstance()::addAttack($values);
+              $values = array();
+          }
+          $index += 1;
       }
-      fclose($handle);
+        fclose($handle);
     }
 ?>
