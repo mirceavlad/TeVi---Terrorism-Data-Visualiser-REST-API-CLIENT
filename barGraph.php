3419639ac8680b362aac65038bd2e8c87775e959
MariaSandru27 <you@example.com>
2020-05-30 00:13:35 +0300

    Year Based Stats

--- barGraph.php
+++ barGraph.php
@@ -155,6 +155,78 @@
          ?>
          }
 
+         function drawYearStats() {
+         /* Accepting and seperating comma seperated values */
+         
+         <?php
+         
+            $weapon = "Any";
+            $country = "Any";
+
+            $year = array();
+            $value = array();
+            $jsonString = file_get_contents("http://localhost/WebTechnologiesProj/attacksDataServiceProvider.php/?flag=3");
+            //deserialization to array
+            $resultsArray = json_decode($jsonString, true);
+            //getting the array size
+            $responseSize = $resultsArray["dataSz"];
+            
+
+           
+            for($i = 0; $i < $responseSize ; $i ++) {
+            $year[$i] = $resultsArray[$i]["iyear"];
+            }
+            
+
+            //remove duplicates
+            $year = array_unique($year);
+            //sort alphabetically
+            sort($year);
+            //put the option in the select
+            
+            
+            for($i = 0; $i < sizeof($year) ; $i ++) {
+             
+                $curyear = str_replace(' ', '%20', $year[$i]);
+                $jsonStringAttacksNo = 
+                file_get_contents("http://localhost/WebTechnologiesProj/attacksDataServiceProvider.php/?flag=4&country=$country&year=$curyear&weapon=$weapon");
+                $resultsArrayAttacksNo = json_decode($jsonStringAttacksNo, true);
+                $nr = $resultsArrayAttacksNo[0]["NR"];
+                $value[$i] = $nr;
+             
+               
+            }
+            
+            
+         echo "
+        
+         var canvas = document.getElementById('myCanvas');
+         var ctx = canvas.getContext('2d');
+         
+         var width = 40; //bar width
+         var X = 50; // first bar position 
+         var base = 200;
+         ";
+         for ($i = 0; $i < sizeof($year); $i ++) {
+             echo "ctx.fillStyle = '#008080'; 
+             var h = ".$value[$i].";
+             ctx.fillRect(X, canvas.height - h, width, h);
+              
+             X +=  width+15;
+
+             ctx.fillStyle = '#4da6ff';
+
+             var currentyear = \"".$year[$i]."\";
+             ctx.fillText(currentyear, X - 50 , canvas.height - h - 10);";
+             
+         }
+
+            
+             echo "ctx.fillStyle = '#000000';
+             ctx.fillText('Scale X : '
+         +canvas.width+' Y : '+canvas.height,7800,10500);";
+         ?>
+         }
          function reset(){
           var canvas = document.getElementById('myCanvas');
           var ctx = canvas.getContext('2d');
@@ -166,6 +238,7 @@
     
     <input type="button" value="Country Stats" name="submit" onclick="draw()">
     <input type="button" value="Weapon Stats" name="submit" onclick="drawWeaponStats()">
+    <input type="button" value="Year Stats" name="submit" onclick="drawYearStats()">
     <input type="button" value="Clear" name="Clear" onclick="reset()">
       <canvas id="myCanvas" width="7800" height="10500"
  style=" border:1px="" solid="" #c3c3c3;"="">
