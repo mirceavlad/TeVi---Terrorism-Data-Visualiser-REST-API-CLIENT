034f4abb774e79a56692faa6106b67fca354716b
MariaSandru27 <you@example.com>
2020-06-08 14:00:53 +0300

    PNG Export

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
     <div id="butoane">
       <button class="btn btn1">CSV</button>
       <button class="btn btn2">SVG</button>
-      <button class="btn btn3">PNG</button>
+      <a id="save" onclick="savePNG()"><button class="btn btn3">PNG</button>
 
     </div>
   </div>
@@ -81,7 +81,53 @@
       document.getElementById("searchside").style.width = "0";
       document.getElementById("listside").style.width = "0";
       document.getElementById("saveside").style.width = "0";
+ 
+   }
+
+   function savePNG(){
+     //maria
+      <?php
+      $country="Any";
+      $year="Any";
+      $weapon="Any"; 
+
+
+      $jsonString = file_get_contents("http://localhost/WebTechnologiesProj/attacksDataServiceProvider.php/?flag=1&country=$country&year=$year&weapon=$weapon");
+
+      $resultsArray = json_decode($jsonString, true);
+
+      $responseSize = $resultsArray["dataSz"];
+
+    $long = array();
+    $lat = array();
+
+    for($i = 0; $i < $responseSize ; $i ++) {
+      $long[$i]=$resultsArray[$i]["longitude"];
+      $lat[$i]=$resultsArray[$i]["latitude"];
+      
     }
+    
+    $totalMarkers = 'markers=color:red%7Clabel:%7C'.$lat[0].','.$long[0];
+    /*$image = file_get_contents
+    ('https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=1&size=1024x2048&key=AIzaSyDtKxEnjmFmud3qf7EQAxdvUyDGrbxhXeo'); */
+    for($i = 1; $i < 100 ; $i ++){
+      if($long[$i]!=NULL && $lat[$i]!=NULL){
+
+       $totalMarkers =  $totalMarkers.'&markers=color:red%7Clabel:%7C'.$lat[$i].','.$long[$i];
+      }
+    }
+
+
+   
+    $image = file_get_contents("https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=1&size=2024x4048&maptype=roadmap&".$totalMarkers."&key=AIzaSyDtKxEnjmFmud3qf7EQAxdvUyDGrbxhXeo");
+    $fp  = fopen('GoogleMap.png', 'w+'); 
+
+    fputs($fp, $image); 
+
+     ?>
+
+   }
+
   </script>
     <?php
     include 'map.php';
