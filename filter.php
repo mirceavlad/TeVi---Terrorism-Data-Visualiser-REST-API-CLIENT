5e4e128df526468b6282dd112bd8a8c26f1ec1b2
Mircea <mirceavlad99@yahoo.com>
2020-06-11 15:35:30 +0300

    Finished documentation

--- filter.php
+++ filter.php
@@ -1,29 +1,7 @@
  <?php
 //mircea
 //filter by selects
-   /* if(isset($_POST["country"]))
-    {$country  = $_POST["country"];
-    }
-    else $country="Any";
-    if(isset($_POST["year"]))
-    {$year  = $_POST["year"];
-    }
-    else $year="Any";
-    if(isset($_POST["weapon"]))
-    {$weapon  = $_POST["weapon"];
-    }
-    else $weapon="Any"; 
-    if(isset($_POST["pages"]))
-    {$nr  = $_POST["pages"];
-    }
-    else $nr=1; 
-      $rez=DBAcess::getInstance()::selectBetween($nr*3500-3500,$country,$year,$weapon);
-      while($atacuri=$rez->fetch_assoc()){
-        $country=$atacuri["country_txt"];
-        $year=$atacuri["iyear"];
-        $weapon=$atacuri["weaptype1_txt"];
-      echo "<li><a class='country'>$country</a><a>$year</a><a>$weapon</a></li>"; 
-      }*/
+   
       if(isset($_POST["country"]))
       {$country  = $_POST["country"];
       }
--- valWeapon.php
+++ valWeapon.php
@@ -2,18 +2,7 @@
 <?php
 //mircea
 //put all weapons in select
-/*$rez=DBAcess::getInstance()::getWeapons();
-$nr=$rez->fetch_assoc();
-echo "<option value=Any selected>Any</option>";
-while($atacuri=$rez->fetch_assoc()){
-    $weapon=$atacuri["weaptype1_txt"];
-    if($weapon==$_POST['weapon'])
-    $isSelected = ' selected="selected"';
-    else{
-        $isSelected = '';
-    }
-    echo "<option value=$weapon".$isSelected.">$weapon</option>";
-}*/
+
 
 //making an array with all the weapons
 for($i = 0; $i < $responseSize ; $i ++) {
