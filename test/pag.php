42cb1a261fce2572df488d679c2e33e69b87e837
Mircea <mirceavlad99@yahoo.com>
2020-06-15 10:33:50 +0300

    Filters test

---
+++ test/pag.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="pag.css">
+<title>
+Lista
+</title>
+</head>
+<body>
+<form method="post" action="cont.php">
+<select id="filter" name="filter">
+<option value="Country">Country</option>
+<option value="Year">Year</option>
+</select>
+<input type="Submit" value="Submit"/>
+</form>
+<form id="filtere">
+<input type="submit" value="Search"/>
+</form>
+<form action="cont1.php">
+<input type="submit" value="RESET"/> 
+</form>
+</body>
+</html>
